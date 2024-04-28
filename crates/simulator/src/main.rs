b829a809e6d61c194984db4ea2cbeb57f6063ddf
robotlab <yanjz21@mails.tsinghua.edu.cn>
2024-04-28 16:17:28 +0800

    ros 可用，喜极而泣阿家人们

--- crates/simulator/src/main.rs
+++ crates/simulator/src/main.rs
@@ -3,11 +3,12 @@ mod plants;
 use crate::plants::{first_order_lti::FirstOrderLTI, plant_trait::Plant};
 use nalgebra as na;
 use rosrust as ros;
+use rosrust_msg as ros_msg;
 
 fn main() {
     ros::init("simuator");
 
-    // let pub_robot_state = ros::publish("robot_state", 100).unwrap();
+    let pub_robot_state = ros::publish("robot_state", 100).unwrap();
 
     let mut plant = FirstOrderLTI::<7>::new();
 
@@ -15,7 +16,8 @@ fn main() {
     print!("ros is ok");
 
     while ros::is_ok() {
-        print!("ros is ok");
+        let mut msg = ros_msg::std_msgs::String::default();
+
         plant.update(na::SVector::from_element(1.0));
         // plant.publish_state(&pub_robot_state);
         rate.sleep();
