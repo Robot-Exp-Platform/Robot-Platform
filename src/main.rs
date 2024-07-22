0b1b17b159400cac3c47b8b85ac4337932c70a6e
yixing <yanjizhou312@163.com>
2024-07-22 18:02:15 +0800

    碎碎念

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,19 @@ mod exp;
 mod thread_manage;
 
 fn main() {
+    // ! 初始化实验，将从 ${CONFIG_PATH}/config.json 中读取配置文件,并生成对应的实验森林，包括机器人树、控制器树、规划器树等
     let exp = exp::Exp::init();
 
+    // ! 主循环家人们，赞美主循环
     loop {
+        // ! 更新任务，将从 ${TASK_PATH}/task.json 中读取任务文件和参数信息文件，并生成对应的任务树
         exp.update_tesk();
+        // ! 所有线程启动启动启动
         exp.thread_manage._start_all();
 
         // TODO: 获取任务结束或者异常的信号
 
+        // ! 所有线程停一会儿，等待下个任务到来
         exp.thread_manage._stop_all();
     }
 }
