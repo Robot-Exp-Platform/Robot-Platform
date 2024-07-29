0801b7c520a1dc1708a9db9547e7ba09206716be
asuka02z <2167119061@qq.com>
2024-07-29 03:07:31 +0000

    一些更新

--- crates/simulator/scripts/test.py
+++ crates/simulator/scripts/test.py
@@ -1,39 +1,56 @@
 import pybullet as p
-import time
 import pybullet_data
-
-print(p.getConnectionInfo())
+import time 
+from pprint import pprint
 
 # 连接物理引擎
-physicsCilent = p.connect(p.GUI)
+use_gui = True
+if use_gui:
+    serve_id = p.connect(p.GUI)
+else:
+    serve_id = p.connect(p.DIRECT)
 
 # 添加资源路径
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-# 设置环境重力加速度
+# 配置渲染机制
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.configureDebugVisualizer(p.COV_ENABLE_TINY_RENDERER, 0)
+p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
+
+# 设置重力，加载模型
 p.setGravity(0, 0, -10)
+_ = p.loadURDF("plane.urdf", useMaximalCoordinates=True)
+robot_id = p.loadURDF("r2d2.urdf", useMaximalCoordinates=True)
 
-# 加载URDF模型，此处是加载蓝白相间的陆地
-planeId = p.loadURDF("plane.urdf")
+available_joints_indexes = [i for i in range(p.getNumJoints(robot_id)) if p.getJointInfo(robot_id, i)[2] != p.JOINT_FIXED]
+wheel_joints_indexes = [i for i in available_joints_indexes if "wheel" in str(p.getJointInfo(robot_id, i)[1])]
 
-# 加载机器人，并设置加载的机器人的位姿
-startPos = [0, 0, 0]
-startOrientation = p.getQuaternionFromEuler([0, 0, 0])
-boxId = p.loadURDF("samurai.urdf", startPos, startOrientation)
+# 预备工作结束，重新开启渲染
+p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
+# 关闭实时模拟步
+p.setRealTimeSimulation(0)
 
-# 按照位置和朝向重置机器人的位姿，由于我们之前已经初始化了机器人，所以此处加不加这句话没什么影响
-p.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
+target_v = 100                   # 电机达到的预定角速度（rad/s）
+max_force = 2                  # 电机能够提供的力，这个值决定了机器人运动时的加速度，太快会翻车哟，单位N
 
-# 开始一千次迭代，也就是一千次交互，每次交互后停顿1/240
-for i in range(1000):
+for i in range(5000):
     p.stepSimulation()
-    time.sleep(1 / 240)
-
-# 获取位置与方向四元数
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print("-" * 20)
-print(f"机器人的位置坐标为:{cubePos}\n机器人的朝向四元数为:{cubeOrn}")
-print("-" * 20)
+    p.setJointMotorControlArray(
+        bodyUniqueId=robot_id,
+        jointIndices=wheel_joints_indexes,
+        controlMode=p.VELOCITY_CONTROL,
+        targetVelocities=[target_v for _ in wheel_joints_indexes],
+        forces=[max_force for _ in wheel_joints_indexes]
+    )
+    location, _ = p.getBasePositionAndOrientation(robot_id)
+    p.resetDebugVisualizerCamera(
+        cameraDistance=3,
+        cameraYaw=110,
+        cameraPitch=-30,
+        cameraTargetPosition=location
+    )
+    time.sleep(1 / 240)            # 模拟器一秒模拟迭代240步
 
 # 断开连接
-p.disconnect()
+p.disconnect(serve_id)
