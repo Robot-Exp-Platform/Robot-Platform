48d57aac49c0cc68a7a7febd9e57f8baaacad9b9
asuka <2167119061@qq.com>
2024-08-07 07:34:31 +0000

    一些测试

--- crates/simulator/scripts/main.py
+++ crates/simulator/scripts/main.py
@@ -14,11 +14,20 @@ if __name__ =="__main__":
     Req = ZMQ_REQ(ZMQ_HOST)
     
     panda1 = Panda([0.5,0,0])
-    panda2 = Panda([-0.5,0,0])
+    period = 0
+    velocities = [0, 0, 0, 0, 0, 0, 0]
+    print("+"*20)
+    panda1.print_panda_info()
+    print("+"*20)
     while True:
-        p.stepSimulation()
+        print("*"*20)
+        panda1.print_panda_state()
+        print("*"*20)
         joint_positions, joint_velocities = panda1.get_joints_state()
         Req.send_array(joint_positions + joint_velocities)
         period, velocities = Req.receive_command()
         print("period: ", period)
         print("joint velocities: ", velocities)
+        panda1.set_joints_velocity(velocities)
+        for i in range(int(period * 240)):
+            p.stepSimulation()
