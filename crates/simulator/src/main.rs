52544de69972e8e1d1273636c108e8f7242ca99d
asuka <2167119061@qq.com>
2024-08-14 08:51:24 +0000

    rust-python通讯

--- crates/simulator/src/main.rs
+++ crates/simulator/src/main.rs
@@ -8,6 +8,7 @@ use robot::robots::panda::Panda;
 use simulator::{simulators::bullet::Bullet, Simulator};
 use task_manager::ros_thread::ROSThread;
 
+
 fn main() {
     let robot = Panda::new_panda("panda_1".to_string(), "/robot".to_string());
     let mut simulator = Bullet::<Panda, 7>::new(
@@ -22,17 +23,16 @@ fn main() {
     controller_command_queue.push(ControlCommand::Joint(vec![
         2.02244, -4.08431, 6.07565, -2.43899, 5.4941, -4.27498, -1.38176,
     ]));
-    controller_command_queue.push(ControlCommand::JointWithPeriod(
+    controller_command_queue.push(ControlCommand::TauWithPeriod(
         1.0 / 240.0,
         vec![
             2.02244, -4.08431, 6.07565, -2.43899, 5.4941, -4.27498, -1.38176,
         ],
     ));
-
+    
     simulator.set_controller_command_queue(controller_command_queue);
 
     simulator.init();
-
     loop {
         simulator.update();
         sleep(Duration::from_secs_f64(0.5));
