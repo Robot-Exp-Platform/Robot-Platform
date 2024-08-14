52402c31941bed3e7db513a7af49a8fa25e9ecbe
yixing <yanjizhou312@163.com>
2024-08-14 18:10:27 +0800

    接下来就是直面 cfs 了

--- crates/sensor/src/sensor_trait.rs
+++ crates/sensor/src/sensor_trait.rs
@@ -1,4 +1,5 @@
 use crate::sensors::obstacle_releaser::ObstacleReleaser;
+use message::collision_object::CollisionObject;
 
 pub enum Sensor {
     ObstacleReleaser(ObstacleReleaser),
@@ -8,6 +9,12 @@ impl Sensor {
     pub fn get_name(&self) -> &String {
         match self {
             Sensor::ObstacleReleaser(obstacle_releaser) => &obstacle_releaser.name,
+        }
+    }
+
+    pub fn get_collision(&self) -> Vec<CollisionObject> {
+        match self {
+            Sensor::ObstacleReleaser(obstacle_releaser) => obstacle_releaser.get_collision(),
         }
     }
 }
