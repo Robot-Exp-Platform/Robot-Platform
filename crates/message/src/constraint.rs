4619a068022c170d703b7639ae51bd1c3e207554
yixing <yanjizhou312@163.com>
2024-08-15 16:55:00 +0800

    整理了cfs约束

--- crates/message/src/constraint.rs
+++ crates/message/src/constraint.rs
@@ -1,8 +1,34 @@
-use serde::Deserialize;
+#[derive(Debug, Default)]
+pub enum Constraint<'a> {
+    #[default]
+    NoConstraint, // 无约束
+    Zero, // 零约束：x = 0
 
-#[derive(Debug, Deserialize)]
-pub enum Constraint {
-    Equality(Vec<f64>, f64),   // 线性等式约束：ax = b
-    Inequality(Vec<f64>, f64), // 线性不等式约束：ax <= b
-    Bound(Vec<f64>, Vec<f64>), // 变量的上下界约束：l <= x <= u
+    AffineSpace,                     // 仿射空间约束：Ax = b
+    EpigraphSquaredNorm(f64),        // 二次范数上确界约束：||x||^2 <= b
+    FiniteSet(Vec<&'a [f64]>),       // 有限集约束：x in {x1, x2, ..., xn}
+    Halfspace(&'a [f64], f64),       // 半空间约束：ax <= b
+    Hyperplane(&'a [f64], f64),      // 超平面约束：ax = b
+    Rectangle(&'a [f64], &'a [f64]), // 矩形约束：a <= x <= b
+    Simplex,                         // 单纯形约束：x in Δ
+    SecondOrderCone,                 // 二阶锥约束
+
+    Intersection(Vec<Constraint<'a>>),                 // 交集约束
+    Union(Vec<Constraint<'a>>),                        // 并集约束
+    CartesianProduct(Vec<usize>, Vec<Constraint<'a>>), // 笛卡尔积约束
+}
+
+impl<'a> Constraint<'a> {
+    pub fn push(&mut self, int: usize, con: Constraint<'a>) {
+        match self {
+            Constraint::CartesianProduct(index, constraint) => {
+                index.push(int);
+                constraint.push(con);
+            }
+            Constraint::Intersection(index) => {
+                index.push(con);
+            }
+            _ => panic!("Only CartesianProduct and Intersection can push"),
+        }
+    }
 }
--- crates/planner/src/planners/cfs.rs
+++ crates/planner/src/planners/cfs.rs
@@ -1,9 +1,9 @@
 use crossbeam::queue::SegQueue;
+use message::constraint::Constraint;
 use nalgebra as na;
 use serde::Deserialize;
 use serde_json::{from_value, Value};
 // use serde_yaml::{from_value, Value};
-// use optimization_engine::constraints::{self, *};
 use std::fs;
 use std::io::{BufWriter, Write};
 use std::sync::{Arc, Mutex, RwLock};
@@ -135,15 +135,57 @@ impl<R: SeriesRobot<N> + 'static, const N: usize> ROSThread for Cfs<R, N> {
         let q = robot_read.get_q_na();
 
         // 执行CFS逻辑
-        let _q_ref_list = utilities::interpolation::<N>(&q, &target, self.params.interpolation);
-        // let constraint_list = constraints::CartesianProduct::new();
-
+        let q_ref_list = utilities::interpolation::<N>(&q, &target, self.params.interpolation);
+        let collision_object = self
+            .node
+            .sensor
+            .as_ref()
+            .unwrap()
+            .read()
+            .unwrap()
+            .get_collision();
         for _ in 0..self.params.iteration_number {
-            // 计算障碍物约束，
+            // 提前分配空间
+            let mut constraint_product = Constraint::CartesianProduct(
+                Vec::with_capacity(self.params.interpolation),
+                Vec::with_capacity(self.params.interpolation),
+            );
+
+            for q_ref in q_ref_list.iter() {
+                let joint_obstacle_distances: Vec<_> = collision_objects
+                    .iter()
+                    .map(|collision| robot_read.get_distance_with_joint(q_ref, &collision))
+                    .collect();
+
+                let joint_obstacle_gradients: Vec<_> = collision_objects
+                    .iter()
+                    .map(|collision| robot_read.get_distance_diff_with_joint(q_ref, &collision))
+                    .collect();
+
+                let mut obstacle_constraint =
+                    Constraint::Intersection(Vec::with_capacity(collision_objects.len()));
+
+                for (distance, gradient) in joint_obstacle_distances
+                    .iter()
+                    .zip(joint_obstacle_gradients.iter())
+                {
+                    obstacle_constraint.push(
+                        N,
+                        Constraint::Halfspace(
+                            -1.0 * gradient,
+                            *distance - (gradient.transpose() * q_ref)[(0, 0)],
+                        ),
+                    );
+                }
+
+                combined_constraints.push(N, obstacle_constraint);
+            }
+
             // 将约束统合为约束类型，约束类型包括关节约束、障碍物约束、起点终点约束
             // 获得优化方程及其梯度
             // 求解优化问题
             // 检查是否收敛，更新 q_ref_list
+            unimplemented!(); // 确保在最终代码中用实际功能替换此行
         }
 
         // 记录 track
