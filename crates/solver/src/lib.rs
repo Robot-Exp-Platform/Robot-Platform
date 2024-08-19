4c918c03c963ff73086c0729000f69daf3edc9f2
yixing <yanjizhou312@163.com>
2024-08-19 15:30:37 +0800

    不知道为什么但是好了

--- crates/solver/src/lib.rs
+++ crates/solver/src/lib.rs
@@ -1,2 +1,32 @@
 pub mod solver_trait;
 pub mod solvers;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn osqp_check() {
+        use osqp::{CscMatrix, Problem, Settings};
+
+        // Define problem data
+        let p = &[[4.0, 1.0], [1.0, 2.0]];
+        let q = &[1.0, 1.0];
+        let a = &[[1.0, 1.0], [1.0, 0.0], [0.0, 1.0]];
+        let l = &[1.0, 0.0, 0.0];
+        let u = &[1.0, 0.7, 0.7];
+
+        // Extract the upper triangular elements of `p`
+        let p = CscMatrix::from(p).into_upper_tri();
+
+        // Disable verbose output
+        let settings = Settings::default().verbose(false);
+
+        // Create an OSQp problem
+        let mut prob = Problem::new(p, q, a, l, u, &settings).expect("failed to setup problem");
+
+        // Solve problem
+        let result = prob.solve();
+
+        // print the solution
+        println!("{:?}", result.x().expect("failed to solve problem"));
+    }
+}
