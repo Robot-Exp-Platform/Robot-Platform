b94bfb888c43d620ca8b6f66d2978c620e2e8ee5
yixing <yanjizhou312@163.com>
2024-08-28 10:39:40 +0800

    修改控制指令消息类型

--- crates/message/src/control_command.rs
+++ crates/message/src/control_command.rs
@@ -4,6 +4,10 @@ use serde::{Deserialize, Serialize};
 pub enum ControlCommand {
     Joint(Vec<f64>),
     JointWithPeriod(f64, Vec<f64>),
+    JointVelocity(Vec<f64>, Vec<f64>),
+    JointVelocityWithPeriod(f64, Vec<f64>, Vec<f64>),
+    JointVelocityAcceleration(Vec<f64>, Vec<f64>, Vec<f64>),
+    JointVelocityAccelerationWithPeriod(f64, Vec<f64>, Vec<f64>, Vec<f64>),
     Tau(Vec<f64>),
     TauWithPeriod(f64, Vec<f64>),
 }
